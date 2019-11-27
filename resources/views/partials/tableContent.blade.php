88b342fc04636cdfae3fa727c6f4f286cc9e0a01
César Pinho <cesar98alexandre@gmail.com>
2019-11-27 23:47:07 +0000

    Fix collapse rows

--- resources/views/partials/tableContent.blade.php
+++ resources/views/partials/tableContent.blade.php
@@ -1,16 +1,18 @@
 @isset($orders)
     @foreach ($orders as $order)
-        <a @empty($dispatch) href="#row-id-{{ $order['id'] }}" @endempty data-toggle="collapse" role="button" aria-expanded="false">
-            <div class="row text-center py-2">
-                <div class="col-2">{{ $order['id'] }}</div>
-                <div class="col-4">{{ $order['order_id'] }}</div>
-                <div class="col-2">{{ $order['owner'] }}</div>
-                <div class="col-3">{{ $order['date'] }}</div>
-                <div class="col-1">
-                    <button class="btn btn-outline-secondary"></button>
+        <div class="row text-center py-2">
+            <a class="col-11" @empty($dispatch) href="#row-id-{{ $order['id'] }}" @endempty data-toggle="collapse" role="button" aria-expanded="false">
+                <div class="row justify-content-around">
+                    <div class="col-2">{{ $order['id'] }}</div>
+                    <div class="col-4">{{ $order['order_id'] }}</div>
+                    <div class="col-2">{{ $order['owner'] }}</div>
+                    <div class="col-3">{{ $order['date'] }}</div>
                 </div>
+            </a>
+            <div class="col-1">
+                <button class="btn btn-outline-secondary"></button>
             </div>
-        </a>
+        </div>
         @empty($dispatch)
             @include('partials.subTableContent', [
                 'items' => $order['items'],
@@ -22,19 +24,21 @@
 
 @isset($waves)
     @foreach ($waves as $wave)
-        <a href="#row-id-{{$wave['id']}}" data-toggle="collapse" role="button" aria-expanded="false">
-            <div class="row text-center py-2">
-                <div class="col-{{ $clerk? '2' : '3'}}">{{$wave['id']}}</div>
-                <div class="col-3">{{$wave['num_orders']}}</div>
-                <div class="col-3">{{$wave['num_products']}}</div>
-                <div class="col-3">{{$wave['date']}}</div>
-                @if ($clerk)
-                    <div class="col-1">
-                        <button class="btn btn-outline-secondary"></button>
-                    </div>
-                @endif
-            </div>
-        </a>
+        <div class="row text-center py-2">
+            <a class="col-{{ $clerk? '11' : '12'}}" @empty($packing) href="#row-id-{{$wave['id']}}" @endempty data-toggle="collapse" role="button" aria-expanded="false">
+                <div class="row justify-content-around">
+                    <div class="col-{{ $clerk? '2' : '3'}}">{{$wave['id']}}</div>
+                    <div class="col-3">{{$wave['num_orders']}}</div>
+                    <div class="col-3">{{$wave['num_products']}}</div>
+                    <div class="col-3">{{$wave['date']}}</div>
+                </div>
+            </a>
+            @if ($clerk)
+                <div class="col-1">
+                    <button class="btn btn-outline-secondary"></button>
+                </div>
+            @endif
+        </div>
         @isset($wave['orders'])
             @include('partials.subTableContent', [
                 'id' => $wave['id'],
