1baefc20cb931ea74c5f5c7dac25a91014213ff7
jm.rocha@fe.up.pt <jm.rocha@fe.up.pt>
2019-12-07 14:31:52 +0000

    Retrieve undispatched orders from DB instead of hardcoded ones

--- app/Dispatching.php
+++ app/Dispatching.php
@@ -25,41 +25,25 @@ class Dispatching extends Model
     protected $primaryKey = 'id';
 
     /**
-     * Get the sales order associated with the dispatch.
-     *
-     * @return HasOne
-     */
-    public function salesOrder() {
-        return $this->hasOne('App\SalesOrders');
-    }
-
-    /**
      * Get the sales orders that weren't dispatched yet.
      *
      * @return array
      */
     public static function undispatched() {
-        // todo: get active dispatching records, and for each one retrieve the salesOrder id. Return associative array
-        // with that info.
-        return [
-                   [
-                       'id' => '4',
-                       'order_id' => 'ay3s678-8df8d9-cvk2kfd4',
-                       'owner' => 'C0004',
-                       'date' => '2019-07-24',
-                   ],
-                   [
-                       'id' => '7',
-                       'order_id' => 'ay3s678-8df8d9-cvk2kfd4',
-                       'owner' => 'C0004',
-                       'date' => '2019-07-24',
-                   ],
-                   [
-                       'id' => '8',
-                       'order_id' => 'ay3s678-8df8d9-cvk2kfd4',
-                       'owner' => 'C0004',
-                       'date' => '2019-07-24',
-                   ]
-               ];
+        $res = [];
+
+        $dispatchOrders = Dispatching::all();
+
+        foreach ($dispatchOrders as $dispatchOrder) {
+            $so = SalesOrders::find($dispatchOrder->sales_order_id);
+
+            array_push($res, [
+               'id' => $so->id,
+               'owner' => $so->client,
+               'date' => $so->date
+            ]);
+        }
+
+        return $res;
     }
 }
