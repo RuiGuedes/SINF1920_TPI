eb60936d0a4a9f67a62ec1b1b7981b2a4bd0f280
César Pinho <cesar98alexandre@gmail.com>
2019-12-08 03:28:00 +0000

    Create function to create a new packing tuple

--- app/Packing.php
+++ app/Packing.php
@@ -15,4 +15,15 @@ class Packing extends Model
      * @var string
      */
     protected $table = 'packing';
+
+    /**
+     * @param String $wave_id
+     */
+    public static function insertPackingWave(String $wave_id)
+    {
+        $packing = new Packing();
+        $packing->picking_wave_id = $wave_id;
+        $packing->save();
+        return $packing->id;
+    }
 }
