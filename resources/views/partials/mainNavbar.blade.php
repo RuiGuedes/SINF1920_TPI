945067ea8df166b6a96af95c1b76e5b5c2337f09
César Pinho <cesar98alexandre@gmail.com>
2019-12-13 15:59:06 +0000

    Fix names and numbers align

--- resources/views/partials/mainNavbar.blade.php
+++ resources/views/partials/mainNavbar.blade.php
@@ -3,7 +3,13 @@
         <a href="{{route('layout')}}">Layout</a>
     </div>
     <div class="col-5 col-sm-3 col-md-3 col-xl-2 text-center">
-        <a class="company-name" href="{{route('manager-sales-orders')}}">TPI</a>
+        <a class="company-name"
+            @if(\App\User::isManager(\Illuminate\Support\Facades\Auth::id()))
+                href="{{route('manager-sales-orders')}}"
+            @else
+                href="{{route('picking-waves')}}"
+            @endif
+        >TPI</a>
     </div>
     <div class="logo-line col text-right align-self-start">
         <a href="{{ route('logout') }}">Logout</a>
--- resources/views/partials/subTableContent.blade.php
+++ resources/views/partials/subTableContent.blade.php
@@ -1,14 +1,14 @@
 <div id="row-id-{{$id}}" class="collapse sub-container row">
     @isset($list_orders)
         <div class="container p-0">
-            <div class="sub-header row text-center py-2 ml-5">
+            <div class="sub-header row text-center py-2">
                 <div class="col-4">Document ID</div>
                 <div class="col-3">Client ID</div>
                 <div class="col-4">Date</div>
             </div>
 
             @foreach ($list_orders as $order)
-                <div class="row text-center py-2 ml-5">
+                <div class="row text-center py-2">
                     <div class="col-4">{{ $order['id'] }}</div>
                     <div class="col-3">{{ $order['owner'] }}</div>
                     <div class="col-4">{{ $order['date'] }}</div>
@@ -22,22 +22,22 @@
     @endisset
 
     @empty($list_orders)
-        <div class="container ml-5 px-0 py-1">
+        <div class="container px-0 py-1">
             <div class="sub-header row text-center py-2">
                 <div class="col-2">Product ID</div>
-                <div class="col-4">Name</div>
-                <div class="col-2">Zone</div>
-                <div class="col-2">Quantity</div>
-                <div class="col-2">@isset($packing) Qnt Picked @endisset @empty($packing) Stock @endempty</div>
+                <div class="col-5 text-left">Name</div>
+                <div class="col-1">Zone</div>
+                <div class="col-2 pr-5 text-right">Quantity</div>
+                <div class="col-2 pr-5 text-right">@isset($packing) Qnt Picked @endisset @empty($packing) Stock @endempty</div>
             </div>
             
             @foreach ($items as $item)
                 <div class="row text-center py-2">
                     <div class="col-2">{{ $item['id'] }}</div>
-                    <div class="col-4">{{ $item['description'] }}</div>
-                    <div class="col-2">{{ $item['zone'] }}</div>
-                    <div class="col-2">{{ $item['quantity'] }}</div>
-                    <div class="col-2">@isset($packing){{ $item['picked_qnt'] }}@endisset @empty($packing){{ $item['stock'] }}@endempty</div>
+                    <div class="col-5 text-left">{{ $item['description'] }}</div>
+                    <div class="col-1">{{ $item['zone'] }}</div>
+                    <div class="col-2 pr-5 text-right">{{ $item['quantity'] }}</div>
+                    <div class="col-2 pr-5 text-right">@isset($packing){{ $item['picked_qnt'] }}@endisset @empty($packing){{ $item['stock'] }}@endempty</div>
                 </div>
             @endforeach
         </div>
